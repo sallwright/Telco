fd6bb50ee549894c2a25fa49f547f2a3162cca91
Stephen Allwright <stephenallwright@gmail.com>
2018-08-11 13:33:38 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Creating a model from Telco ccustomer data',
+    author='Stephen Allwright',
+    license='MIT',
+)
